d5b9c36c48ebb1c62d9271e1c06f9d864237b407
Fiesta87 <jb@pommeret.fr>
2018-04-27 21:57:42 -0400

    mise à jour de la BD

--- jdr.sql
+++ jdr.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 26 avr. 2018 à 23:27
+-- Généré le :  sam. 28 avr. 2018 à 01:57
 -- Version du serveur :  5.7.19
 -- Version de PHP :  7.1.9
 
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `attribut` (
   `nom` varchar(200) NOT NULL,
   `valeur` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `attribut`
@@ -57,7 +57,15 @@ INSERT INTO `attribut` (`id`, `idFiche`, `idAttributParent`, `nom`, `valeur`) VA
 (18, 6, 14, 'Antenne de communication', 1),
 (19, 6, -1, 'Implant auxiliaire', 1),
 (20, 6, 19, 'Œil Bionique', 1),
-(21, 6, 19, 'Acuité auditive', 1);
+(21, 6, 19, 'Acuité auditive', 1),
+(22, 7, -1, 'work in progress', 1),
+(23, 9, -1, 'Chapeau', 1),
+(24, 9, 23, 'Couleur', 1),
+(25, 9, 23, 'Taille', 1),
+(26, 9, -1, 'Costume', 1),
+(27, 9, -1, 'Chaussures', 1),
+(28, 9, 27, 'Lacets', 1),
+(29, 9, 26, 'Cravate', 1);
 
 -- --------------------------------------------------------
 
@@ -93,7 +101,7 @@ CREATE TABLE IF NOT EXISTS `fiche` (
   `description` varchar(500) NOT NULL,
   `idUser` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `fiche`
@@ -102,7 +110,10 @@ CREATE TABLE IF NOT EXISTS `fiche` (
 INSERT INTO `fiche` (`id`, `nom`, `description`, `idUser`) VALUES
 (4, 'Ma super fiche', 'Une super fiche !', 8),
 (5, 'Fiche Lorem Ipsum', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a metus lacinia, gravida urna sit amet, dignissim dolor. Aenean iaculis felis eget massa dignissim fringilla. Sed interdum lorem odio, in fringilla ipsum vehicula ut. Ut sollicitudin scelerisque libero, sit amet laoreet velit ornare vestibulum. Fusce quis erat eget lorem vulputate mollis posuere in urna. Aliquam erat volutpat. Cras eget libero at purus venenatis luctus vitae non quam.', 8),
-(6, 'Cyborg', 'Une bonne base de départ pour un cyborg !', 7);
+(6, 'Cyborg', 'Une bonne base de départ pour un cyborg !', 7),
+(7, 'Fiche de Test', 'je sais pas quoi écrire comme description...', 8),
+(9, 'Chapelier Fou', 'Fou mais gentil !', 9),
+(10, 'Chat du Cheshire', 'Il est invisible !', 9);
 
 -- --------------------------------------------------------
 
@@ -139,7 +150,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` varchar(200) NOT NULL,
   `salt` binary(16) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `user`
@@ -147,7 +158,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 INSERT INTO `user` (`id`, `username`, `email`, `password`, `salt`) VALUES
 (7, 'amesk', 'azerty@gmail.com', 'fe88defbf8fd208632e26c37055d2872de0f03575109dec5e0aaf4dc0fea06c55dffc7ebc12636957c897da9b601d267fd92d20d24ef93794dbec204ae7335a3', 0xa023fd6aeb71f7255bbea7810c98a295),
-(8, 'Bob', 'bob@gmail.com', '1e68b7a95b5c174fe4f06329b2ae785f4b0bed10c8831b24547292d952f48467a6db8eb066c64b2feabbf8929bff2ae1d88202848370c069c431cf51812a721d', 0x469b88445d4ac69011998e49637adf12);
+(8, 'Bob', 'bob@gmail.com', '1e68b7a95b5c174fe4f06329b2ae785f4b0bed10c8831b24547292d952f48467a6db8eb066c64b2feabbf8929bff2ae1d88202848370c069c431cf51812a721d', 0x469b88445d4ac69011998e49637adf12),
+(9, 'Alice', 'alice@gmail.com', '5c13389c27be758368f3e194a14cb9e18d16b1da1921d9bbf92cf71ed960f10baab7dc26e85ffa9f8e3752c7e395f73b013bc0ac7f16e4dcc7e79f86b3c7a1fa', 0x14dc6cae8eff3eec46698b268f6fa3af);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
